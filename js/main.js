6467b9020d08d60c68a7b9367a9919b8bd07a6ad
dargavavid <dargavavid@gmail.com>
2018-01-10 23:21:20 +0100

    Add some global variables

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var $document = jQuery(document);
+
+var gifFieldStr = `<div class="gif-section-field">
+            <input type="text" class="field-imgsrc field-input" placeholder="Kép src...">
+            <input type="text" class="field-gifsrc field-input" placeholder="Gif src...">
+            <input type="text" class="field-descr field-input" placeholder="Leírás...">
+            <button class="delete-field-button">X Mező törlése</button>
+        </div>`;
+
+// $addFieldButton.on("click", function() {
+//     var $gifField = jQuery(gifFieldStr);
+//     $(this).parent().append($gifField);
+// });
