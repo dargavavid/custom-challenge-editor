91f2dd4e6a69990733bf38786b759b8d1a1eb067
dargavavid <dargavavid@gmail.com>
2018-01-11 00:08:31 +0100

    Add insert content button, add style

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ var gifFieldStr = `<div class="gif-section-field">
             <input type="text" class="field-imgsrc field-input" placeholder="Kép src...">
             <input type="text" class="field-gifsrc field-input" placeholder="Gif src...">
             <input type="text" class="field-descr field-input" placeholder="Leírás...">
-            <button class="delete-field-button">X Mező törlése</button>
+            <button class="delete-field-button">X</button>
         </div>`;
 
 function handleAddSection() {
